467a2d16d9d6e3fddcbe7f9b6213994164c8b705
Andrew Misplon <Andrew@Andrews-MacBook-Pro.local>
2016-01-16 12:04:52 -0800

    Cleaned up child theme enqueues.

--- functions.php
+++ functions.php
@@ -1,11 +1,9 @@
 <?php
 
 /**
-* Enqueue styles.
-*/
-add_action( 'wp_enqueue_scripts', 'puro_child_styles' );
-
-function puro_child_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+ * Enqueue the parent theme stylesheet.
+ */
+function puro_child_enqueue_parent_style() {
+    wp_enqueue_style( 'puro-parent-style', get_template_directory_uri() . '/style.css' );
 }
+add_action( 'wp_enqueue_scripts', 'puro_child_enqueue_parent_style', 8 );
