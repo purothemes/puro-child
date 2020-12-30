a1e372bce843e05b2b5fad54bb83b4ca0d5c32a0
Andrew Misplon <amisplon@gmail.com>
2020-12-30 14:51:09 +0200

    Spaces to tabs

--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@
  * Enqueue the parent theme stylesheet.
  */
 function puro_child_enqueue_parent_style() {
-    wp_enqueue_style( 'puro-parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'puro-parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'puro_child_enqueue_parent_style', 8 );
